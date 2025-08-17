71ee11b1264d5acca56c2b688b434a3187d46586
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-17 17:19:44 +0545

    push

--- pms/settings.py
+++ pms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t(v)6aymn*4m==)1w4$=q*7$+3iw*p^5^2qnquap2$h(2-&2pf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','pms.hunchhadigital.com.np']
+ALLOWED_HOSTS = ['*','pms.hunchhadigital.com.np', 'localhost']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
