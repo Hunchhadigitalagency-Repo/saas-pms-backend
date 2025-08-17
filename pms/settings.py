357e1a6c25ff156a02e27d27294c621f8ab12fce
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-17 17:14:01 +0545

    push

--- pms/settings.py
+++ pms/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*','pms.hunchhadigital.com.np']
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
     "http://localhost:5173",
     "http://demo.localhost",
     "https://pms.hunchhadigital.com.np"
@@ -36,7 +37,9 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://pms.hunchhadigital.com.np",
     "https://*.pms.hunchhadigital.com.np",
-    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5173"
+
 ]
 
 CORS_ALLOW_METHODS = (
