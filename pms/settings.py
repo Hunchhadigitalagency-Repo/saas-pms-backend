1ce32ea5df02249b0450e5a498378cef1d876195
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-17 17:24:52 +0545

    push

--- pms/settings.py
+++ pms/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-t(v)6aymn*4m==)1w4$=q*7$+3iw*p^5^2qnquap2$h(2-&2pf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','pms.hunchhadigital.com.np', 'localhost']
+ALLOWED_HOSTS = [
+    "pms.hunchhadigital.com.np",
+    ".pms.hunchhadigital.com.np",  # allows all subdomains
+    "localhost",
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
@@ -35,7 +39,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://pms.hunchhadigital.com.np",
     "https://*.pms.hunchhadigital.com.np",
-    "http://localhost:5173"
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_METHODS = (
@@ -48,6 +52,12 @@ CORS_ALLOW_METHODS = (
 )
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.pms\.hunchhadigital\.com\.np$",
+]
+
+CSRF_COOKIE_SECURE = True
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
