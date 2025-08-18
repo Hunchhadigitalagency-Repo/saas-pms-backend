df656e9ea336194eb227f2ddff734a1dc0ed6e9d
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-18 19:30:29 +0545

    ready to push

--- user/adapters/viewsets/auth_viewset.py
+++ user/adapters/viewsets/auth_viewset.py
@@ -137,6 +137,10 @@ class AuthViewSet(viewsets.ViewSet):
         )(request, *args, **kwargs)
 
     def logout(self, request, *args, **kwargs):
-        return auth_views.LogoutView.as_view(
-            next_page=reverse_lazy('logout_done')
-        )(request, *args, **kwargs)
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
