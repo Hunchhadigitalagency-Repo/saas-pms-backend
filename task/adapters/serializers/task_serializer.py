e8f839443e20939c9512b6a5aca84b362218790a
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-18 17:04:11 +0545

    push

--- task/adapters/serializers/task_serializer.py
+++ task/adapters/serializers/task_serializer.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from task.models import Task
 from django.contrib.auth.models import User
+from project.models import Project
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'name',]
+
 class TaskSerializer(serializers.ModelSerializer):
     assigned_to = UserSerializer(many=True, read_only=True)
+    project = ProjectSerializer(read_only=True)
 
     class Meta:
         model = Task
