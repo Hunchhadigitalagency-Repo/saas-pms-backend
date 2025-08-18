4c9512c57033c64e754db559785a5474fabce7a5
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-18 19:40:55 +0545

    push

--- user/adapters/viewsets/auth_viewset.py
+++ user/adapters/viewsets/auth_viewset.py
@@ -143,4 +143,4 @@ class AuthViewSet(viewsets.ViewSet):
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"error": str(e)})
