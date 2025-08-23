0395d079eacb44f28501bc40f752d3f24705cb08
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-08-23 21:27:50 +0545

    push

--- work_items/adapters/serializers/work_items_serializer.py
+++ work_items/adapters/serializers/work_items_serializer.py
@@ -1,12 +1,19 @@
+from backend.user.models import UserProfile
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from project.models import Project
 from ...models import WorkItems
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('profile_picture')
+
 class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'profile')
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
