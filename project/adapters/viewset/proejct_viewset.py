30a82e3ea10e8518843e50fcd6303e7d237bed5c
vneet2057 <divin828@gmail.com>
2025-12-16 13:20:34 +0545

    feat: Override ProjectViewSet create and update methods to utilize distinct serializers for request processing and response generation.

--- project/adapters/viewset/proejct_viewset.py
+++ project/adapters/viewset/proejct_viewset.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, filters
+from rest_framework.response import Response
+from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
 from project.models import Project
 from project.adapters.serializers.project_serializer import ProjectSerializer, OnGoingProjectSerializer, ProjectWriteSerializer
@@ -26,6 +28,30 @@ class ProjectViewSet(viewsets.ModelViewSet):
         if self.action in ['create', 'update', 'partial_update']:
             return ProjectWriteSerializer
         return ProjectSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Use write serializer for validation and saving
+        write_serializer = self.get_serializer(data=request.data)
+        write_serializer.is_valid(raise_exception=True)
+        instance = write_serializer.save()
+        
+        # Use read serializer for response to include team_members and all fields
+        read_serializer = ProjectSerializer(instance)
+        headers = self.get_success_headers(read_serializer.data)
+        return Response(read_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        
+        # Use write serializer for validation and saving
+        write_serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        write_serializer.is_valid(raise_exception=True)
+        instance = write_serializer.save()
+        
+        # Use read serializer for response to include team_members and all fields
+        read_serializer = ProjectSerializer(instance)
+        return Response(read_serializer.data)
 
 class OngoingProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.filter(status='active').order_by('-id')
