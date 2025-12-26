ddc808ff88d883788cf34248f6835ee1332d87c8
vneet2057 <divin828@gmail.com>
2025-12-26 14:44:06 +0545

    WI-44 #inprogress building the feature and testing

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
+from backend.project.adapters.viewset.project_activity_viewset import ProjectActivityLogViewSet
 from rest_framework.routers import DefaultRouter
-from project.adapters.viewset.proejct_viewset import ProjectViewSet, OngoingProjectViewSet, ProjectActivityLogViewSet
+from project.adapters.viewset.proejct_viewset import ProjectViewSet, OngoingProjectViewSet
 
 
 router = DefaultRouter()
