265293bc8bb9f5558ce396f2325e66df22c2d685
vneet2057 <divin828@gmail.com>
2025-12-27 13:37:15 +0545

    fixing the team member url issue after adding and updating the project

--- project/adapters/serializers/project_serializer.py
+++ project/adapters/serializers/project_serializer.py
@@ -5,16 +5,31 @@ from user.models import UserProfile
 
 
 class ProjectUserProfileSerializer(serializers.ModelSerializer):
+    profile_picture = serializers.SerializerMethodField()
+
     class Meta:
         model = UserProfile
         fields = ('profile_picture',)
 
+    def get_profile_picture(self, obj):
+        if obj.profile_picture:
+            request = self.context.get('request')
+            if request is not None:
+                return request.build_absolute_uri(obj.profile_picture.url)
+            return obj.profile_picture.url
+        return None
+
 class ProjectMemberUserSerializer(serializers.ModelSerializer):
-    profile = ProjectUserProfileSerializer()
+    profile = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name', 'profile')
+
+    def get_profile(self, obj):
+        if hasattr(obj, 'profile') and obj.profile:
+            return ProjectUserProfileSerializer(obj.profile, context=self.context).data
+        return None
 
 class ProjectMemberSerializer(serializers.ModelSerializer):
     user = ProjectMemberUserSerializer(read_only=True)
